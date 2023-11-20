e98cfdff3fd1ea393b8f821fd885fabb6970431a
Boubacar Kina <boubacarkina1@gmail.com>
2023-11-20 08:35:07 +0100

    datas

---
+++ datas.js
@@ -0,0 +1,64 @@
+let datas = {
+    cards: [
+        {
+            titre: "Cake Item",
+            prix: 5,
+            image: "./img/cake1.webp"
+        },
+        {
+            titre: "Cake Item",
+            prix: 5,
+            image: "./img/cake2.webp"
+        },
+        {
+            titre: "Cake Item",
+            prix: 5,
+            image: "./img/cake3.webp"
+        },
+        {
+            titre: "Cupcake Item",
+            prix: 8,
+            image: "./img/cupcake1.jpeg"
+        },
+        {
+            titre: "Cupcake Item",
+            prix: 8,
+            image: "./img/cupcake2.jpeg"
+        },
+        {
+            titre: "Cupcake Item",
+            prix: 8,
+            image: "./img/cupcake3.jpeg"
+        },
+        {
+            titre: "Sweet Item",
+            prix: 6,
+            image: "./img/sweet1.webp"
+        },
+        {
+            titre: "Sweet Item",
+            prix: 6,
+            image: "./img/sweet2.webp"
+        },
+        {
+            titre: "Sweet Item",
+            prix: 6,
+            image: "./img/sweet3.webp"
+        },
+        {
+            titre: "Doughnut Item",
+            prix: 7,
+            image: "./img/doughnut1.jpeg"
+        },
+        {
+            titre: "Doughnut Item",
+            prix: 7,
+            image: "./img/doughnut1.jpeg"
+        },
+        {
+            titre: "Doughnut Item",
+            prix: 7,
+            image: "./img/doughnut1.jpeg"
+        },
+    ]
+}
